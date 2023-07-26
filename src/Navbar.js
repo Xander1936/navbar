d7df8ed5620aebd96645bb0c6e768ff28aee6cd9
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2023-07-26 16:31:41 +0100

    Second commit: Navbar simple Toggle Button

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,37 +4,46 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav>
       <div className='nav-center'>
-        <img src={logo} alt='logo' />
-        <button className='nav-toggle'>
-          <FaBars />
-        </button>
-      </div>
-      <div className='links-container' show-container>
-        <ul className='links'>
-          {links.map((link) => {
-            const { id, url, text } = link;
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button 
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className='links-container show-container'>
+            <ul className='links'>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>
+                      {text}
+                    </a>
+                  </li>
+                )
+              } )}
+            </ul>
+          </div>
+        )}
+        <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
             return (
               <li key={id}>
-                <a href={url}>
-                  {text}
-                </a>
+                <a href={url}> {icon} </a>
               </li>
             )
-          } )}
+          })}
         </ul>
-      </div>
-      <div className='social-icons'>
-        {social.map((socialIcon) => {
-          const { id, url, icon } = socialIcon;
-          return (
-            <li key={id}>
-              <a href={url}> {icon} </a>
-            </li>
-          )
-        })}
       </div>
     </nav>
   )
