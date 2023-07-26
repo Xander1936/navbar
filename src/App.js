a79589bfcd320bda2f89f687f0135d61d7441492
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2023-07-26 14:39:57 +0100

    First commit: Intro + Structure + Links Data

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
+
+// <></>
